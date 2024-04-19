1cf375b0aafe9e7519c94714b282c51c1c127803
Raymond Jia <jia.raymond19@gmail.com>
2024-04-19 00:00:19 -0400

    summmer24 rework

---
+++ priority_queue.py
@@ -0,0 +1,130 @@
+#####################################################
+# CS 6601 - Assignment 0
+# priority_queue.py
+#####################################################
+
+# DO NOT ADD OR REMOVE ANY IMPORTS FROM THIS FILE
+
+class PriorityQueue(object):
+    """
+    A queue structure where each element is served in order of priority.
+
+    Elements in the queue are popped based on the priority with higher priority
+    elements being served before lower priority elements.  If two elements have
+    the same priority, they will be served in the order they were added to the
+    queue.
+
+    Traditionally priority queues are implemented with heaps, but there are any
+    number of implementation options.
+
+    (Hint: take a look at the module heapq)
+
+    You may add extra helper functions within the class if you find them necessary.
+
+    Attributes:
+        queue (list): Nodes added to the priority queue.
+    """
+
+    def __init__(self):
+        """Initialize a new Priority Queue."""
+
+        self.queue = []
+
+    def pop(self):
+        """
+        Pop top priority node from queue.
+
+        Returns:
+            The node with the highest priority.
+        """
+
+        # TODO: finish this function!
+        raise NotImplementedError
+
+    def remove(self, node):
+        """
+        Remove a node from the queue.
+
+        Hint: You might require this in ucs. However, you may
+        choose not to use it or to define your own method.
+
+        Args:
+            node (tuple): The node to remove from the queue.
+        """
+        
+        # We will not test this function, implementation and desired behavior is up to your discretion
+        # Some students find that this function is useful for them in Assignment 1
+        raise NotImplementedError
+
+    def __iter__(self):
+        """Queue iterator."""
+
+        return iter(sorted(self.queue))
+
+    def __str__(self):
+        """Priority Queue to string."""
+
+        return 'PQ:%s' % self.queue
+
+    def append(self, node):
+        """
+        Append a node to the queue.
+
+        Args:
+            node (tuple): Comparable Object to be added to the priority queue.
+            Provided in the form of (int priority, any type payload)
+        """
+
+        # TODO: finish this function!
+        raise NotImplementedError
+        
+    def __contains__(self, key):
+        """
+        Containment Check operator for 'in'
+
+        Args:
+            key: The key to check for in the queue.
+
+        Returns:
+            True if key is found in queue, False otherwise.
+        """
+
+        return key in [n[-1] for n in self.queue]
+
+    def __eq__(self, other):
+        """
+        Compare this Priority Queue with another Priority Queue.
+
+        Args:
+            other (PriorityQueue): Priority Queue to compare against.
+
+        Returns:
+            True if the two priority queues are equivalent.
+        """
+
+        return self.queue == other.queue
+
+    def size(self):
+        """
+        Get the current size of the queue.
+
+        Returns:
+            Integer of number of items in queue.
+        """
+
+        return len(self.queue)
+
+    def clear(self):
+        """Reset queue to empty (no nodes)."""
+
+        self.queue = []
+
+    def top(self):
+        """
+        Get the top item in the queue.
+
+        Returns:
+            The first item stored in the queue.
+        """
+
+        return self.queue[0]
